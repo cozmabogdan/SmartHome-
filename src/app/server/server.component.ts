8f81d674f7707be5a998a86c1d08236ef1add75c
Bogdan Cozma <bogdan.cozma1990@gmail.com>
2021-03-13 09:21:04 +0100

    Updated server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,9 +10,29 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color!: string;
+  buttonText!: string;
+
   @Input() serverInput!: Server;
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline)
   }
 
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66bb6a';
+      this.buttonText = 'Turn Off'
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#ff6b6b';
+      this.buttonText = 'Turn On';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ':', onlineStatus);
+    this.setServerStatus(!onlineStatus);
+  }
 }
